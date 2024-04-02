93c869fcac7665bc4aca61a09711247cf524437a
Xusheng Ai <xai@node0006.palmetto.clemson.edu>
2024-04-02 14:42:08 -0400

    add util

---
+++ diffusion/diffusion_util.py
@@ -0,0 +1,75 @@
+import math
+import torch as th
+import torch.nn as nn
+
+
+def mean_flat(tensor):
+    """
+    Take the mean over all non-batch dimensions.
+    """
+    return tensor.mean(dim=list(range(1, len(tensor.shape))))
+
+
+def normal_kl(mean1,logvar1,mean2,logvar2):
+    """
+    Compute the KL divergence between two gaussians.
+    Shapes are automatically broadcasted, so batches can be compared to 
+    scalars, among other use cases. 
+    """
+    tensor = None
+    for obj in (mean1,logvar1,mean2,logvar2):
+        if isinstance(obj,th.Tensor):
+            tensor = obj
+            break
+    assert tensor in not None, "at least one argument must be a Tensor"
+
+    # Force variances to be Tensors. Broadcasting helps convert scalars to
+    # Tensors, but it does not work for th.exp().
+    logvar1,logvar2 = [
+        x if isinstance(x, th.Tensor) else th.tensor(x).to(tensor)
+        for x in (logvar1,logvar2)
+    ]
+
+    return 0.5 * (
+        -1.0
+        + logvar2
+        - logvar1
+        + th.exp(logvar1 - logvar2)
+        + ((mean1 - mean2) ** 2) * th.exp(-logvar2)
+    )
+
+    
+def approx_standard_normal_cdf(x):
+    """
+    A fast approximation of the cumulative distribution function of the 
+    standard normal.
+    """
+    return 0.5 * (1.0 + th.tanh(np.sqrt(2.0/np.pi) * (x + 0.044715 * th.pow(x,3))))
+
+
+def discretized_gaussian_log_likelihood(x, *, means, log_scales):
+    """
+    Compute the log-likelihood of a Gaussian distribution sidcretizing
+
+    :param x: the target data, assuming data rescaled to the range[-1,1]
+    :param log_scales: the Gaussian log stddev Tensor.
+    :return: a tensor like x of log probabilities 
+    """
+
+    assert x.shape == means.shape == log_scales.shape
+    centered_x = x - mean 
+    inv_stdv = th.exp(-log_scales)
+    plus_in = inv_stdv * (centered_x + 1.0 / 255.0)
+    cdf_plus = approx_standard_normal_cdf(plus_in)
+    min_in = inv_stdv * (centered_x - 1.0 / 255.0)
+    cdf_mid = approx_standard_normal_cdf(min_in)
+    log_cdf_plus = th.log(cdf_plus.clamp(min=1e-12))
+    log_one_minus_cdf_min = th.log((1.0 - cdf_min).clamp(min=1e-12))
+    cdf_delta = cdf_plus - cdf_min
+    log_probs = th.where(
+        x < -0.999,
+        log_cdf_plus,
+        th.where(x>0.999, log_one_minus_cdf_min, th.log(cdf_delta.clamp(min=1e-12))),
+    )
+    assert log_probs.shape == x.shape
+    return log_probs
