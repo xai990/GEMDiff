40b3893c69674c90394d57a5d60960114ce900a1
Xusheng Ai <xai@node0006.palmetto.clemson.edu>
2024-04-30 11:57:03 -0400

    changed back to plt plot

--- diffusion/script_util.py
+++ diffusion/script_util.py
@@ -141,12 +141,14 @@ def showdata(dataset,
     # dir to save the plot image
     assert isinstance(dir,str)
     os.makedirs(dir,exist_ok=True)
-    reducer = umap.UMAP(n_neighbors=15, min_dist=0.1, n_components=2, random_state=41)
-    colors = ['blue','orange']
+    reducer = umap.UMAP(n_neighbors=60, min_dist=0.3, n_components=2, random_state=41)
+    added_labels = set()
     if schedule_plot == "forward": 
+        colors = ['blue','orange']
+        labels = ['normal','tumor']
         assert diffusion is not None
         rows = num_shows // cols
-        fig,axs = plt.subplots(rows,cols,figsize=(28,3))
+        fig,axes = plt.subplots(rows,cols,figsize=(28,3))
         plt.rc('text',color='black')
         data , y = dataset[:][0], dataset[:][1]['y']
         data = th.from_numpy(data).float()
@@ -156,21 +158,72 @@ def showdata(dataset,
             t = th.full((data.shape[0],),i*num_steps//num_shows)
             x_i = diffusion.q_sample(data,t)
             q_i = reducer.fit_transform(x_i)
+            added_labels.clear()
             for ele in y:
                 index_mask = (ele==y)
                 if np.any(index_mask):
-                    axs[j,k].scatter(q_i[index_mask,0],
+                    label = labels[ele] if ele not in added_labels else None
+                    axes[j,k].scatter(q_i[index_mask,0],
                                 q_i[index_mask,1],
-                                label = ele, 
+                                label = label, 
                                 color=colors[ele],
                                 edgecolor='white')
-            axs[j,k].set_axis_off()
-            axs[j,k].set_title('$q(\mathbf{x}_{'+str(i*num_steps//num_shows)+'})$')
+                    if label:
+                        added_labels.add(ele)
+            axes[j,k].set_axis_off()
+            axes[j,k].set_title('$q(\mathbf{x}_{'+str(i*num_steps//num_shows)+'})$')
+        handles, labels = axes[0, 0].get_legend_handles_labels()  # Get handles and labels from any one subplot
+        fig.legend(handles, labels, loc='upper left', ncol=3)
         plt.savefig(f"{dir}/dataset-forward_{data.shape[-1]}.png")
         plt.close()
     elif schedule_plot == "origin":
-        fig,ax = plt.subplots()      
+        # fig,ax = plt.subplots()      
         data , y = dataset[:][0], dataset[:][1]['y']
+        # Define the range of parameters you want to explore
+        n_neighbors_options = [15, 30, 60, 90, 120, 150, 180]
+        min_dist_options = [0.1, 0.3, 0.6, 0.9]
+        color_map = ['blue','orange']
+        
+        labels = ['normal','tumor']
+        # Setup the subplot grid
+        fig, axes = plt.subplots(nrows=len(n_neighbors_options), ncols=len(min_dist_options), figsize=(15, 15))
+        for i, n_neighbors in enumerate(n_neighbors_options):
+            for j, min_dist in enumerate(min_dist_options):
+                # Create UMAP model
+                reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, random_state=41)
+                embedding = reducer.fit_transform(data)
+                # Plot
+                ax = axes[i,j]  # Get a specific subplot axis
+                added_labels.clear()
+                for ele in y:
+                    index_mask = (ele==y)
+                    if np.any(index_mask):
+                        label = labels[ele] if labels[ele] not in added_labels else None
+                        ax.scatter(embedding[index_mask,0],
+                                    embedding[index_mask,1],
+                                    label = label, 
+                                    color=color_map[ele],
+                                    edgecolor='white')
+                        if label:
+                            added_labels.add(label)
+                # scatter = ax.scatter(embedding[:, 0], embedding[:, 1], c=y, cmap='viridis', s=5)
+                # ax.set_title(f'n_neighbors={n_neighbors}, min_dist={min_dist}')
+                if i == 0:
+                    ax.set_xlabel(f'{min_dist}')
+                    ax.xaxis.set_label_position('top')
+                if j == 0:
+                    ax.set_ylabel(f'{n_neighbors}')
+                ax.axes.xaxis.set_ticklabels([])
+                ax.axes.yaxis.set_ticklabels([])
+                # ax.axis('off')
+        handles, labels = axes[0, 0].get_legend_handles_labels()
+        fig.legend(handles, labels, loc='upper left', ncol=3)
+        fig.text(0.5, 1,'min_dist',ha='center',)
+        fig.text(0.0, 0.5,'n_neighbors', va='center', rotation='vertical')
+        plt.tight_layout()
+        plt.subplots_adjust(wspace=0., hspace=0.)
+        
+        """
         q_i = reducer.fit_transform(data)
         for ele in y:
             index_mask = (ele==y)
@@ -181,6 +234,7 @@ def showdata(dataset,
                             color=colors[ele],
                             edgecolor='white')
         ax.axis('off')
+        """
         plt.savefig(f"{dir}/dataset_{data.shape[-1]}.png")
         plt.close()
     elif schedule_plot == "reverse":
@@ -188,9 +242,69 @@ def showdata(dataset,
         data_f, y_f = synthesis_data["arr_0"], synthesis_data["arr_1"]
         # stack the data together for overarching patterns 
         data_merged = np.vstack([data_r,data_f])
-        q_i = reducer.fit_transform(data_merged)
-        q_r = q_i[:len(y_r)]
-        q_f = q_i[len(y_r):]
+        
+        # create the umap parameters sweep plot 
+        n_neighbors_options = [15, 30, 60, 90, 120, 150, 180]
+        min_dist_options = [0.1, 0.3, 0.6, 0.9]
+        color_map = ['blue','orange','cyan','blueviolet']
+        labels = ['real_normal','real_tumor','fake_normal', 'fake_turmor']
+        # Setup the subplot grid
+        fig, axes = plt.subplots(nrows=len(n_neighbors_options), ncols=len(min_dist_options), figsize=(15, 15))
+        for i, n_neighbors in enumerate(n_neighbors_options):
+            for j, min_dist in enumerate(min_dist_options):
+                # Create UMAP model
+                reducer = umap.UMAP(n_neighbors=n_neighbors, min_dist=min_dist, random_state=41)
+                q_i = reducer.fit_transform(data_merged)
+                q_r = q_i[:len(y_r)]
+                q_f = q_i[len(y_r):]
+                # Plot
+                ax = axes[i, j]  # Get a specific subplot axis
+                added_labels.clear()
+                for ele in y_r:
+                    index_mask = (ele==y_r)
+                    if np.any(index_mask):
+                        label = labels[ele] if labels[ele] not in added_labels else None
+                        ax.scatter(q_r[index_mask,0],
+                                    q_r[index_mask,1],
+                                    label = label, 
+                                    color=color_map[ele],
+                                    edgecolor='white')
+                        if label:
+                            added_labels.add(label)
+
+                for ele in y_f:
+                    index_mask = (ele==y_f)
+                    if np.any(index_mask):
+                        label = labels[ele+2] if labels[ele+2] not in added_labels else None
+                        ax.scatter(q_f[index_mask,0],
+                                    q_f[index_mask,1],
+                                    label = label, 
+                                    color=color_map[ele+2],
+                                    edgecolor='white')
+                        if label:
+                            added_labels.add(label)
+
+                # scatter = ax.scatter(embedding[:, 0], embedding[:, 1], c=y, cmap='viridis', s=5)
+                # ax.set_title(f'n_neighbors={n_neighbors}, min_dist={min_dist}')
+                if i == 0:
+                    ax.set_xlabel(f'{min_dist}')
+                    ax.xaxis.set_label_position('top')
+                if j == 0:
+                    ax.set_ylabel(f'{n_neighbors}')
+                ax.axes.xaxis.set_ticklabels([])
+                ax.axes.yaxis.set_ticklabels([])
+                # ax.axis('off')
+        handles, labels = axes[0, 0].get_legend_handles_labels()
+        fig.legend(handles, labels, loc='upper left', ncol=3)
+        fig.text(0.5, 1,'min_dist',ha='center',)
+        fig.text(0.0, 0.5,'n_neighbors', va='center', rotation='vertical')
+        plt.tight_layout()
+        plt.subplots_adjust(wspace=0., hspace=0.)
+        # plt.show()
+        plt.legend(loc="upper left")
+        plt.savefig(f"{dir}/UMAP_plot_realvsfake_{data_r.shape[-1]}.png")
+        plt.close()
+        """
         # Create a Plotly figure
         fig = go.Figure()
         # plot the synthesis data
@@ -223,7 +337,7 @@ def showdata(dataset,
 
         # Show the figure
         fig.write_html(f"{dir}/UMAP_plot_realvsfake_{data_r.shape[-1]}.html")
-
+        """
     else:
         raise NotImplementedError(f"unknown schedule plot:{schedule_plot}")
 
