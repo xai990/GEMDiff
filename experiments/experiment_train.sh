342d79b1b73b8d30434a59ad33cea609f8d71e3f
Xusheng Ai <xai@node0006.palmetto.clemson.edu>
2024-07-25 14:07:23 -0400

    updated the code

--- experiments/experiment_train.sh
+++ experiments/experiment_train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
 CONFIG_DIR="configs"
-SLURM_TEMPLATE="ddpm_train.sh"
-# GENE_DIR="geneset/perturb"
-JOB_DIR="jobs"
+SLURM_TEMPLATE="experiments/ddpm_train.sh"
+GENE_PATH="None"
+JOB_DIR="jobs/"
 
 for CONFIG_FILE in "$CONFIG_DIR"/*.yaml; do 
     # extrac the config parameters 
@@ -15,6 +15,7 @@ for CONFIG_FILE in "$CONFIG_DIR"/*.yaml; do
     LOG_PATH="log/${JOB_NAME}"
     # replace the key for each sbatch job 
     sed -e "s|{{CONFIG_PATH}}|\"$CONFIG_PATH\"|" -e "s|{{JOB_NAME}}|$JOB_NAME|" -e "s|{{LOG_PATH}}|\"$LOG_PATH\"|" "$SLURM_TEMPLATE" > "$SLURM_SCRIPT"
+    sed -i "s|{{GENE_PATH}}|\"$GENE_PATH\"|" "$SLURM_SCRIPT"
     # submit the qsub job 
     sbatch "$SLURM_SCRIPT"
     mv "$SLURM_SCRIPT" "$JOB_DIR"
