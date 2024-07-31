8c94395073b715afa75e33177d76758604692d77
Xusheng Ai <xai@node0006.palmetto.clemson.edu>
2024-07-31 10:19:30 -0400

    added the ss and umap plot

--- scripts/gene.py
+++ scripts/gene.py
@@ -36,7 +36,7 @@ def main(args):
                 class_cond=config.data.cond,
                 gene_set = args.gene_set,
     )
-    score = get_silhouettescore(train)
+    score = get_silhouettescore(train,n_neighbors = config.umap.n_neighbors,min_dist = config.umap.min_dist)
     experiment = args.gene_set if args.gene_set else "Random"
     
     logger.log(f"{experiment} experiemnt of silhouette score: {score}")
@@ -45,7 +45,7 @@ def main(args):
     # umap plot 
     showdata(train,
             dir = get_blob_logdir(),
-            schedule_plot = "balance",
+            schedule_plot = "origin",
             n_neighbors = config.umap.n_neighbors,
             min_dist = config.umap.min_dist,
             gene_set = args.gene_set,
@@ -75,6 +75,6 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     # parser.add_argument("--config", type=str, default="configs/mrna_8.yaml")
     parser.add_argument("--dir", type=str, default=None)
-    parser.add_argument("--gene_set", type=str, default=None)
+    parser.add_argument("--gene_set", type=str, default="Random")
     args = parser.parse_args()
     main(args)
