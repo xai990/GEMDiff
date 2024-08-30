685a081c093de94be205d98f77fa19aeb16f4714
Xusheng Ai <xai@node0006.palmetto.clemson.edu>
2024-08-30 16:06:24 -0400

    added std

--- diffusion/script_util.py
+++ diffusion/script_util.py
@@ -487,7 +487,9 @@ def filter_gene(real, perturb):
     assert real.shape == perturb.shape, f'The datashape of real and perturbed are different'
     # Calculate the difference between the corresponding gene
     logger.log(f"The real data {real.mean(axis=0)}-- script_util")
+    logger.log(f"The real data {real.std(axis=0)}-- script_util")
     logger.log(f"The perturb data {perturb.mean(axis=0)}-- script_util")
+    logger.log(f"The perturb data {perturb.std(axis=0)}-- script_util")
     differences = np.abs(real - perturb).mean(axis=0)
     logger.log(f"The differences between real and perturb data {differences} -- script_util")
     
