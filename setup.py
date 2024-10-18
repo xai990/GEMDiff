92ac32c3dcb17ea8f921f23cb6aab92d8f8ece97
Xusheng Ai <xai@node0006.palmetto.clemson.edu>
2024-10-18 17:38:51 -0400

    updated the packages

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup 
 
 setup(
-        name="DDPM-mRNA-augmentation-light",
+        name="GEMDiff",
         py_modules=["diffusion"],
-        install_requires=["blobfile>=1.0.5", "torch"],
+        install_requires=["blobfile>=1.0.5", "torch", "einops", "omegaconf", "umap-learn[plot]"],
 )
