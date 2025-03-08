10671b3b66de49661405b39c00f2c2a379eb1f52
Xusheng Ai <xai@node0006.palmetto.clemson.edu>
2025-03-07 20:19:02 -0500

    added description about each script

--- scripts/perturb.py
+++ scripts/perturb.py
@@ -1,3 +1,21 @@
+"""
+This script performs perturbation analysis using a trained diffusion model. 
+
+It loads a pre-trained diffusion model, perturbs gene expression data (either training or testing data) from one class to another,
+visualizes the perturbed data, and identifies the most perturbed genes.
+
+The script relies on configuration files to specify data paths, model parameters, and other settings.
+
+Usage:
+    python script_name.py --config configs/your_config.yaml --model_path path/to/your/model.pth --dir path/to/log/dir [--valid] [--gene_set your_gene_set]
+
+    --config: Path to the configuration YAML file.
+    --model_path: Path to the pre-trained model checkpoint.
+    --dir: Directory to save logs and results.
+    --valid: Use the validation (test) dataset instead of the training dataset.
+    --gene_set: The gene set to use for filtering.
+"""
+
 import argparse
 from omegaconf import OmegaConf
 from diffusion import logger, dist_util
@@ -30,9 +48,11 @@ def main(args):
     logger.configure(dir=args.dir)
     logger.log("**********************************")
     logger.log("log configure")
+    # Set up distributed training environment
     dist_util.setup_dist()
     logger.log(f"device information:{dist_util.dev()}")
     logger.log("Load the config...")
+    # Create and merge configuration files
     basic_conf = create_config()
     input_conf = OmegaConf.load(args.config)
     config = OmegaConf.merge(basic_conf, input_conf)
@@ -46,152 +66,21 @@ def main(args):
                     gene_set = args.gene_set,
                     data_filter=config.data.filter,
     )
-    # train_data, test_data = load_data(data_dir = config.data.data_dir,
-    #                 gene_selection = config.model.feature_size,
-    #                 class_cond=config.model.class_cond,
-    #                 gene_set = args.gene_set,
-    # )
     # separate the tumor and normal data
     train_N, train_T = sample_screen(train_data)
     test_N, test_T = sample_screen(test_data)
-    # set the model param
-    
+    # Set model configuration parameters
     logger.log("creating model and diffusion ... ")
     logger.info(f"The model feature size is : {config.model.feature_size}")
     config.model.patch_size = config.model.feature_size
     config.model.n_embd = config.model.patch_size * 8
-    # config.model.n_embd = config.model.patch_size * 4
     logger.info(config)
     model_config = OmegaConf.to_container(config.model, resolve=True)
     diffusion_config = OmegaConf.to_container(config.diffusion, resolve=True)
-
-    # if args.model_dir is None:
-    #     model_N, diffusion = create_model_and_diffusion(**model_config, **diffusion_config)
-    #     model_N.to(dist_util.dev())
-    #     logger.info(model_N)
-    #     ema_N = deepcopy(model_N).to(dist_util.dev())  # Create an EMA of the model for use after training
-    #     requires_grad(ema_N, False)
-    #     logger.log("train the target model:")  
-        
-    #     schedule_sampler = create_named_schedule_sampler(config.train.schedule_sampler, diffusion)
-    #     if config.model.class_cond:
-    #         trainN = LabelGeneDataset(train_N,0)
-    #     else:
-    #         trainN = train_N
-    #     loader_N = data_loader(trainN,
-    #                     batch_size=config.train.batch_size,            
-    #     ) 
-    #     optimizer_N = th.optim.Adam(model_N.parameters(),lr=config.train.lr)
-    #     update_ema(ema_N, model_N, decay=0)  # Ensure EMA is initialized with synced weights
-    #     model_N.train()  # important! This enables embedding dropout for classifier-free guidance
-    #     ema_N.eval()  # EMA model should always be in eval mode
-    #     for epoch in range(config.train.num_epoch):
-    #         for idx,batch_x in enumerate(loader_N):
-    #             batch, cond = batch_x
-    #             target_y = {}
-    #             if config.model.class_cond:
-    #                 target_y = {k: v.to(dist_util.dev()) for k, v in cond.items()}
-    #             # logger.debug(f"target_t is : {target_y}")
-    #             batch_size = batch.shape[0]
-    #             t, _ = schedule_sampler.sample(batch.shape[0], dist_util.dev())
-    #             losses = diffusion.loss(model_N,
-    #                                     batch.to(dist_util.dev()),
-    #                                     t.to(dist_util.dev()),
-    #                                     model_kwargs=target_y)
-    #             loss = (losses["loss"]).mean()
-    #             optimizer_N.zero_grad()
-    #             loss.backward()
-    #             th.nn.utils.clip_grad_norm_(model_N.parameters(),1.)
-    #             optimizer_N.step()
-    #             update_ema(ema_N, model_N)
-
-
-    #         if (epoch % config.train.log_interval == 0):
-    #             logger.log(f"The loss is : {loss} at {epoch} epoch")
-    #         # save model checkpoint     
-    #         if(epoch%config.train.save_interval==0) and epoch > 0:
-    #             if dist.get_rank() == 0:
-    #                 checkpoint = {
-    #                     "model": model_N.state_dict(),
-    #                     "ema": ema_N.state_dict(),
-    #                     "opt": optimizer_N.state_dict(),
-    #                     "conf": config
-    #                 }
-    #                 filename = f"model_N{(epoch):05d}.pt"
-    #                 checkpoint_path = os.path.join(get_blob_logdir(), filename)
-    #                 th.save(checkpoint, checkpoint_path)
-    #                 logger.log(f"Saved checkpoint to {checkpoint_path}")
-        
-    #     logger.log("train the source model:")  
-    #     model_T, _ = create_model_and_diffusion(**model_config, **diffusion_config)
-    #     model_T.to(dist_util.dev())
-    #     logger.info(model_T)
-    #     ema_T = deepcopy(model_T).to(dist_util.dev())  # Create an EMA of the model for use after training
-    #     requires_grad(ema_T, False)
-    #     if config.model.class_cond:
-    #         trainT = LabelGeneDataset(train_T,1)
-    #     else:
-    #         trainT = train_T
-    #     loader_T = data_loader(trainT,
-    #                     batch_size=config.train.batch_size,            
-    #     )
-    #     optimizer_T = th.optim.Adam(model_T.parameters(),lr=config.train.lr)
-    #     update_ema(ema_T, model_T, decay=0)  # Ensure EMA is initialized with synced weights
-    #     model_T.train()  # important! This enables embedding dropout for classifier-free guidance
-    #     ema_T.eval()  # EMA model should always be in eval mode
-    #     for epoch in range(config.train.num_epoch):
-    #         for idx,batch_x in enumerate(loader_T):
-    #             batch, cond = batch_x # not consider the label info for now 
-    #             source_y = {}
-    #             if config.model.class_cond:
-    #                 source_y = {k: v.to(dist_util.dev()) for k, v in cond.items()}
-    #             batch_size = batch.shape[0]
-    #             t, _ = schedule_sampler.sample(batch.shape[0], dist_util.dev())
-    #             losses = diffusion.loss(model_T,
-    #                                     batch.to(dist_util.dev()),
-    #                                     t.to(dist_util.dev()),
-    #                                     model_kwargs=source_y)
-    #             loss = (losses["loss"]).mean()
-    #             optimizer_N.zero_grad()
-    #             loss.backward()
-    #             th.nn.utils.clip_grad_norm_(model_T.parameters(),1.)
-    #             optimizer_T.step()
-    #             update_ema(ema_T, model_T)
-
-
-    #         if (epoch % config.train.log_interval == 0):
-    #             logger.log(f"The loss is : {loss} at {epoch} epoch")
-    #         # save model checkpoint     
-    #         if(epoch%config.train.save_interval==0) and epoch > 0:
-    #             if dist.get_rank() == 0:
-    #                 checkpoint = {
-    #                     "model": model_T.state_dict(),
-    #                     "ema": ema_T.state_dict(),
-    #                     "opt": optimizer_T.state_dict(),
-    #                     "conf": config
-    #                 }
-    #                 filename = f"model_T{(epoch):05d}.pt"
-    #                 checkpoint_path = os.path.join(get_blob_logdir(), filename)
-    #                 th.save(checkpoint, checkpoint_path)
-    #                 logger.log(f"Saved checkpoint to {checkpoint_path}")
-    # else:
-        # load the model from pretrain
-        
-        # model_N, diffusion = create_model_and_diffusion(**model_config, **diffusion_config)
-        # ema_N = deepcopy(model_N)
-        # model_T, _ = create_model_and_diffusion(**model_config, **diffusion_config)
-        # ema_T = deepcopy(model_T)
-        # state_dict = th.load(f"{args.model_dir}/model_N40000.pt")
-        # ema_N.load_state_dict(state_dict["ema"])
-        # ema_N.to(dist_util.dev())
-        # ema_N.eval()
-        # state_dict = th.load(f"{args.model_dir}/model_T40000.pt")
-        # ema_T.load_state_dict(state_dict["ema"])
-        # ema_T.to(dist_util.dev())
-        # ema_T.eval()
-        
+    # Create model and diffusion process
     model, diffusion = create_model_and_diffusion(**model_config, **diffusion_config)
     ema= deepcopy(model)
+    # Load model weights from checkpoint (using EMA weights)
     state_dict = th.load(f"{args.model_path}")
     ema.load_state_dict(state_dict["ema"])
     ema.to(dist_util.dev())
@@ -207,9 +96,7 @@ def main(args):
         th.Tensor(test_T).float().to(dist_util.dev()) if args.valid else th.Tensor(train_T).float().to(dist_util.dev()),
         clip_denoised=False,
         model_kwargs=source_label,
-        # device=dist_util.dev(),
     )
-    # logger.debug(f"The len of noise_T is:{len(noise_T)}")
     target_label = {}
     if config.model.class_cond:
         target_label['y'] = th.zeros(test_T.shape[0] if args.valid else train_T.shape[0],device=dist_util.dev(),dtype=th.int32) 
@@ -226,7 +113,6 @@ def main(args):
     
     logger.log("filter the perturbed gene -- 1 std")
     gene_index = filter_gene(test_T if args.valid else train_T, target, config.data.corerate)
-    # logger.log(f"The indentified genes are: {train_data.find_gene(gene_index)} -- 1 standard deviation of the perturbation among all {train_N.shape[1]} gene")
     if args.valid:
         logger.log(f"The indentified genes are: {test_data.find_gene(gene_index)} -- {config.data.corerate} standard deviation of the perturbation among all {test_N.shape[1]} gene")
     else:
